04cc91534eca6ef6b45bd6477422a71331ab0ceb
Nathan Felix <natholas@gmail.com>
2017-03-21 22:34:44 +0100

    removed unused folder

--- app/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/healthcheck', function(req, res) {
-  var response = 'Ok';
-  res.send(response);
-});
-
-module.exports = router;
