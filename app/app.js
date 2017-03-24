46ebc05eba7eb158006dfe5dd426ed9dc4699ae4
Nathan Felix <natholas@gmail.com>
2017-03-25 00:12:11 +0100

    Moved it back..

--- app/app.js
+++ app/app.js
@@ -6,6 +6,24 @@ var app = express();
 
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
+
 app.post('/*', function(req, res) {
   request(req, function(response) {
     res.send(response);
